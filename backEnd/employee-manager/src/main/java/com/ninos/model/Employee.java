7e8712210389bea63a39dc4a60fbe01527e99fab
NinoosMoshi <nalkaton@gmail.com>
2021-12-14 08:56:00 -0500

    Spring Employee class Closes #1

---
+++ backEnd/employee-manager/src/main/java/com/ninos/model/Employee.java
@@ -0,0 +1,28 @@
+package com.ninos.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+}
